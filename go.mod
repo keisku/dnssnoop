ba2226e42339aec8f5f12b67efef0032274532cc
keisku <keisuke.umegaki.630@gmail.com>
2023-01-10 09:36:00 +0000

    initialized the go module

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/keisku/dnssnoop
+
+go 1.19
+
+require github.com/cilium/ebpf v0.9.3
+
+require golang.org/x/sys v0.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import _ "github.com/cilium/ebpf"
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf bpf_prog.c
+
+func main() {
+}
