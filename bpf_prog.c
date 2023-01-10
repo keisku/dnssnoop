5d4e08b600084dd2c18c01089c2a10f5d61d1222
keisku <keisuke.umegaki.630@gmail.com>
2023-01-10 10:30:51 +0000

    better parsing a packet

--- bpf_prog.c
+++ bpf_prog.c
@@ -10,6 +10,8 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
+#include "parsing_helpers.h"
+
 #define MAX_MAP_ENTRIES 16
 
 char __license[] SEC("license") = "Dual MIT/GPL";
@@ -41,42 +43,42 @@ parse_event(struct xdp_md *ctx, struct event *evt)
 	void *data_end = (void *)(long)ctx->data_end;
 	void *data = (void *)(long)ctx->data;
 
-	struct ethhdr *eth = data;
-	if ((void *)(eth + 1) > data_end)
-	{
-		return 0;
-	}
+	/* These keep track of the next header type and iterator pointer */
+	struct hdr_cursor nh;
+	int nh_type;
+	nh.pos = data;
+
+	struct ethhdr *eth;
+	nh_type = parse_ethhdr(&nh, data_end, &eth);
+	if (nh_type < 0)
+		return -1;
 
 	if (eth->h_proto != bpf_htons(ETH_P_IP))
-	{
-		return 0;
-	}
+		return -1;
 
-	// Then parse the IP header.
-	struct iphdr *iph = (void *)(eth + 1);
-	if ((void *)(iph + 1) > data_end)
-	{
-		return 0;
-	}
+	struct iphdr *iph;
+	nh_type = parse_iphdr(&nh, data_end, &iph);
+	if (nh_type < 0)
+		return -1;
+
 	__u16 sport = 0;
 	__u16 dport = 0;
-	if (iph->protocol == IPPROTO_UDP)
+	if (nh_type == IPPROTO_UDP)
 	{
-		struct udphdr *udph = data + sizeof(struct ethhdr) + (iph->ihl * 2);
-		if (udph + 1 > (struct udphdr *)data_end)
-		{
-			return 0;
-		}
+		struct udphdr *udph;
+		nh_type = parse_udphdr(&nh, data_end, &udph);
+		if (nh_type < 0)
+			return -1;
 		sport = bpf_ntohs(udph->source);
 		dport = bpf_ntohs(udph->dest);
 	}
-	if (iph->protocol == IPPROTO_TCP)
+
+	if (nh_type == IPPROTO_TCP)
 	{
-		struct tcphdr *tcph = data + sizeof(struct ethhdr) + (iph->ihl * 4);
-		if (tcph + 1 > (struct tcphdr *)data_end)
-		{
-			return 0;
-		}
+		struct tcphdr *tcph;
+		nh_type = parse_tcphdr(&nh, data_end, &tcph);
+		if (nh_type < 0)
+			return -1;
 		sport = bpf_ntohs(tcph->source);
 		dport = bpf_ntohs(tcph->dest);
 	}
@@ -93,17 +95,16 @@ SEC("xdp")
 int xdp_prog_func(struct xdp_md *ctx)
 {
 	struct event evt;
-	if (!parse_event(ctx, &evt))
-	{
+	if (parse_event(ctx, &evt) < 0)
 		return XDP_PASS;
-	}
+
 	struct event *e;
 	e = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
 	if (!e)
-	{
 		return XDP_PASS;
-	}
+
 	*e = evt;
 	bpf_ringbuf_submit(e, 0);
+
 	return XDP_PASS;
 }
